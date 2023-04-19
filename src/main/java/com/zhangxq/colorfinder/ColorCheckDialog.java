67c72f1fc6dab7a5ff30e875255b2536adce4f70
zhangxq <zhangxiaoqi@ksztone.com>
2023-04-19 15:10:55 +0800

    完成颜色查询功能

--- src/main/java/com/zhangxq/colorfinder/ColorCheckDialog.java
+++ src/main/java/com/zhangxq/colorfinder/ColorCheckDialog.java
@@ -10,23 +10,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Pattern;
 
 public class ColorCheckDialog extends JDialog {
     private static final String COLOR_PATTERN = "^#([0-9a-fA-F]{8}|[0-9a-fA-F]{6})$";
     private JPanel contentPane;
     private JButton btnSearch;
-    private JButton btnAdd;
     private JTextField tfLightColor;
     private JTextField tfNightColor;
-    private JTextField tfResult;
+    private JTextArea taResult;
     private final Project project;
-    private final java.util.List<File> colorFileList = new ArrayList<>();
-    private final List<File> nightColorFileList = new ArrayList<>();
+    private final List<File> allColorFileList = new ArrayList<>();
 
     public ColorCheckDialog(Project project) {
         this.project = project;
@@ -34,29 +30,40 @@ public class ColorCheckDialog extends JDialog {
         setModal(true);
         getRootPane().setDefaultButton(btnSearch);
 
-        setSize(new Dimension(400, 300));
+        setMinimumSize(new Dimension(300, 190));
+        setMaximumSize(new Dimension(300, 190));
         Toolkit kit = Toolkit.getDefaultToolkit();
-        setLocation(kit.getScreenSize().width / 2 - 200, kit.getScreenSize().height / 2 - 150);
+        setLocation(kit.getScreenSize().width / 2 - 150, kit.getScreenSize().height / 2 - 100);
+
+        taResult.setMinimumSize(new Dimension(280, 200));
+        taResult.setEditable(false);
+        taResult.setVisible(false);
 
         btnSearch.addActionListener(e -> {
             String lightColor = tfLightColor.getText().trim();
             String nightColor = tfNightColor.getText().trim();
 
             if (!Pattern.matches(COLOR_PATTERN, lightColor) || !Pattern.matches(COLOR_PATTERN, nightColor)) {
-                NotifyUtil.notify("颜色格式不正确", project);
+                NotifyUtil.notify("颜色格式不正确，示例：#ffffff", project);
                 return;
             }
 
             List<String> result = findColor(lightColor, nightColor);
             if (result.size() > 0) {
-                tfResult.setText(result.get(0));
-            }
-        });
-
-        btnAdd.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
+                setMinimumSize(new Dimension(300, 390));
+                setMaximumSize(new Dimension(300, 390));
+                taResult.setVisible(true);
+                StringBuilder resultContent = new StringBuilder();
+                resultContent.append("查询到以下可用颜色：").append("\n");
+                for (String str : result) {
+                    resultContent.append(str).append("\n");
+                }
+                if (result.size() > 1) {
+                    resultContent.append("\n").append("靓仔，顺便把重复颜色合并一下 (๑•̀ㅂ•́)و✧");
+                }
+                taResult.setText(resultContent.toString());
+            } else {
+                NotifyUtil.notify("未找到", project);
             }
         });
 
@@ -75,14 +82,42 @@ public class ColorCheckDialog extends JDialog {
         if (file.exists()) {
             findFile(file);
         }
-        if (colorFileList.size() > 0 && nightColorFileList.size() > 0) {
-            // 拿到所有白天模式颜色
-            Map<String, String> lightColorMap = getColorMap(colorFileList);
+
+        List<File> lightColorFileList = new ArrayList<>(); // 亮色模式颜色文件列表
+        List<File> nightColorFileList = new ArrayList<>(); // 暗色模式颜色文件列表
+        List<File> singleColorFileList = new ArrayList<>(); // 通用颜色文件列表
+
+        // 第一遍循环过滤出暗色模式颜色文件
+        Set<String> lightNightNames = new HashSet<>();
+        for (File item : allColorFileList) {
+            if (item.getParentFile() != null && item.getParentFile().getName().contains("night")) {
+                nightColorFileList.add(item);
+                lightNightNames.add(item.getName());
+            }
+        }
+        // 第二遍循环过滤出与暗色模式文件对应的亮色模式颜色文件
+        for (File item : allColorFileList) {
+            if (lightNightNames.contains(item.getName()) && !nightColorFileList.contains(item)) {
+                lightColorFileList.add(item);
+            }
+        }
+        // 第三遍循环过滤出其他颜色文件
+        for (File item : allColorFileList) {
+            if (!lightColorFileList.contains(item) && !nightColorFileList.contains(item)) {
+                singleColorFileList.add(item);
+            }
+        }
+
+        // 拿到所有单色颜色
+        Map<String, String> singleColorMap = getColorMap(singleColorFileList);
+        if (lightColorFileList.size() > 0 && nightColorFileList.size() > 0) {
+            // 拿到所有亮色模式颜色
+            Map<String, String> lightColorMap = getColorMap(lightColorFileList);
             // 拿到所有暗色模式颜色
             Map<String, String> nightColorMap = getColorMap(nightColorFileList);
 
-            List<String> matchLightColor = getColorNames(lightColor.toLowerCase(), lightColorMap);
-            List<String> matchNightColor = getColorNames(nightColor.toLowerCase(), nightColorMap);
+            List<String> matchLightColor = getColorNames(lightColor.toLowerCase(), lightColorMap,singleColorMap);
+            List<String> matchNightColor = getColorNames(nightColor.toLowerCase(), nightColorMap, singleColorMap);
             List<String> result = new ArrayList<>();
 
             for (String out : matchLightColor) {
@@ -98,10 +133,23 @@ public class ColorCheckDialog extends JDialog {
         return new ArrayList<>();
     }
 
-    private List<String> getColorNames(String color, Map<String, String> colorMap) {
+    /**
+     * 获取匹配到的色值名称
+     * @param color
+     * @param colorMap
+     * @param singleColorMap
+     * @return
+     */
+    private List<String> getColorNames(String color, Map<String, String> colorMap, Map<String, String> singleColorMap) {
         List<String> result = new ArrayList<>();
         for (String name : colorMap.keySet()) {
-            String value = formatColor(colorMap.get(name).toLowerCase());
+            // 获取色值
+            String value = colorMap.get(name);
+            // 获取真实颜色值，有的颜色是 @color/text_main
+            value = findTrueColor(value, colorMap, singleColorMap).toLowerCase();
+
+            // 对颜色格式化
+            value = formatColor(value);
             color = formatColor(color);
 
             if (value.equals(color)) {
@@ -109,6 +157,30 @@ public class ColorCheckDialog extends JDialog {
             }
         }
         return result;
+    }
+
+    /**
+     * 获取真实颜色值，有的颜色是 @color/text_main
+     * @param color
+     * @return
+     */
+    private String findTrueColor(String color, Map<String, String> colorMap, Map<String, String> singleColorMap) {
+        if (color.startsWith("@color/")) {
+            String name = color.substring(7);
+
+            String resultColor = colorMap.get(name);
+            if (resultColor != null) {
+                return findTrueColor(resultColor, colorMap, singleColorMap);
+            }
+
+            resultColor = singleColorMap.get(name);
+            if (resultColor != null) {
+                return findTrueColor(resultColor, colorMap, singleColorMap);
+            }
+
+            NotifyUtil.notify("未找到" + color, project);
+        }
+        return color;
     }
 
     /**
@@ -156,12 +228,8 @@ public class ColorCheckDialog extends JDialog {
             if (f.isDirectory()) {
                 findFile(f);
             } else {
-                if (f.getName().startsWith("color")) {
-                    if (file.getName().contains("night")) {
-                        nightColorFileList.add(f);
-                    } else {
-                        colorFileList.add(f);
-                    }
+                if (f.getName().contains("color")) {
+                    allColorFileList.add(f);
                 }
             }
         }
